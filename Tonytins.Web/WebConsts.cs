00d479c52248aa49e126895828fe222ce4ff9d2a
Anthony Foxclaw <noreply@tonytins.xyz>
2020-08-19 17:20:29 -0400

    Added license
    - Removed survey
    - Added about page

---
+++ Tonytins.Web/WebConsts.cs
@@ -0,0 +1,8 @@
+// Anthony Leland licenses this file to you under the MIT license.
+namespace Tonytins.Web
+{
+    public struct WebConsts
+    {
+        public const string Name = "Tonytins";
+    }
+}
