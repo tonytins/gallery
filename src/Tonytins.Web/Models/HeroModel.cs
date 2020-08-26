7e8232c4ada67a8eba5b7ace2a367b9ae4338e37
Anthony Foxclaw <noreply@tonytins.xyz>
2020-08-26 16:56:50 -0400

    Ignore null values in Json

--- src/Tonytins.Web/Models/HeroModel.cs
+++ src/Tonytins.Web/Models/HeroModel.cs
@@ -11,7 +11,7 @@ namespace Tonytins.Web.Models
 
         public string Backstory { get; set; } = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et condimentum sem. Curabitur et congue leo. In.";
 
-        public string Image { get; set; } = @"../images/brands/coh-card.png";
+        public string Image { get; set; } = "../images/brands/coh-card.png";
         public string Server { get; set; } = "City of Heroes";
     }
 }
