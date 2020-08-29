fac23f168d8ea51f07751c0ba0370d5d3f92c0ca
Anthony Foxclaw <noreply@tonytins.xyz>
2020-08-29 09:12:57 -0400

    Dark mode toggle
    - New WIP dark mode that doesn't save it's state, at the moment, because I haven't figured that out yet
    - GiftArtComponent renamed to GalleryComponent and made more generic
    - Added Anti-Aidan (was originally known as Dark Aidan)
    - Removed projects list because it wasn't getting updated anymore and there is a Github link in the footer
    - Moved copyright for the gift of Stefen and I into the card itself
    - Moved image resolver in Hero card out of it and into the CoH page

---
+++ src/Tonytins.Web/Models/GalleryModel.cs
@@ -0,0 +1,13 @@
+namespace Tonytins.Web.Models
+{
+    public class GalleryModel
+    {
+        public string Title { get; set; }
+
+        public string Image { get; set; }
+
+        public string Credits { get; set; }
+
+        public string Copyrights { get; set; }
+    }
+}
