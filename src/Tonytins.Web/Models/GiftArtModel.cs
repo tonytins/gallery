a99bbd8470f6e0023aec6a565c0ae692494baa40
Anthony Foxclaw <noreply@tonytins.xyz>
2020-08-29 05:34:55 -0400

    Gift art page!

---
+++ src/Tonytins.Web/Models/GiftArtModel.cs
@@ -0,0 +1,11 @@
+namespace Tonytins.Web.Models
+{
+    public class GiftArtModel
+    {
+        public string Image { get; set; }
+
+        public string Character { get; set; }
+
+        public string Credits { get; set; }
+    }
+}
