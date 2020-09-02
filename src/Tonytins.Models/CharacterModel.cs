26af7da11fd00bc809ec04b65024a4e75665428b
Anthony Foxclaw <noreply@tonytins.xyz>
2020-09-02 05:51:01 -0400

    Added characters model

---
+++ src/Tonytins.Models/CharacterModel.cs
@@ -0,0 +1,9 @@
+namespace Tonytins.Models
+{
+    public class CharacterModel
+    {
+        public string Name { get; set; }
+        public string Descrption { get; set; }
+        public string Page { get; set; }
+    }
+}
