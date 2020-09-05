b07fd56270316e8ae692c720c5cfcf6825ac943f
Anthony Foxclaw <noreply@tonytins.xyz>
2020-09-05 05:37:11 -0400

    Dark and Light card themes are now consts in a struct

--- src/Tonytins.Web/SiteConsts.cs
+++ src/Tonytins.Web/SiteConsts.cs
@@ -1,9 +1,13 @@
 // Anthony Leland licenses this file to you under the MIT license.
+using System.Runtime.InteropServices;
+
 namespace Tonytins.Web
 {
     public struct SiteConsts
     {
         public const string SiteName = "Tonytins";
+
+        public static string Framework => RuntimeInformation.FrameworkDescription;
 
         public static string SiteVersion =>
             $"{ThisAssembly.Git.SemVer.Major}.{ThisAssembly.Git.SemVer.Minor}.{ThisAssembly.Git.SemVer.Patch}-{ThisAssembly.Git.Commit}";
---
+++ src/Tonytins.Web/Themes.cs
@@ -0,0 +1,9 @@
+namespace Tonytins.Web
+{
+    public struct Themes
+    {
+        public const string DarkCards = "bg-secondary text-light";
+
+        public const string Light = "bg-light text-dark";
+    }
+}
