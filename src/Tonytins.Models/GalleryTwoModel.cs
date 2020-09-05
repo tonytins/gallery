b9637a47fdcedd5eb072483496406eb22cbfbbaf
Anthony Foxclaw <noreply@tonytins.xyz>
2020-09-05 08:40:06 -0400

    Gallery v2 header
    - Gallery v2 now includes the number of images within in it if a title is present
    - Art page v2 now also has the gallery title

--- src/Tonytins.Models/GalleryTwoModel.cs
+++ src/Tonytins.Models/GalleryTwoModel.cs
@@ -1,7 +1,11 @@
 namespace Tonytins.Models
 {
+    /// <summary>
+    /// Gallery page
+    /// </summary>
     public class GalleryTwoModel
     {
+        public string Title { get; set; }
         public string Description { get; set; }
         public GalleryModel[] Artwork { get; set; }
     }
