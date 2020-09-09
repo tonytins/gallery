62e77f4a71929170f744a48aba02c49823cbba59
Anthony Foxclaw <noreply@tonytins.xyz>
2020-09-08 23:31:14 -0400

    Removed alert storage

--- src/Tonytins.Web/wwwroot/js/app.js
+++ src/Tonytins.Web/wwwroot/js/app.js
@@ -1,11 +1,8 @@
 $(document).ready(function () {
     var ad = document.getElementById("adex");
-    var support = document.getElementById("ifadblocker");
     if ($("#adex").height() > 0 || $("#ifadblocker").height() > 0) {
         ad.style.display = "block";
-        support.style.display = "none"
     } else {
         ad.style.display = "none";
-        support.style.display = "block"
     }
 });
