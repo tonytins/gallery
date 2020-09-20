aa1da70af41c22e410d00049dc2a526d4245c755
Anthony Leland <noreply@tonytins.xyz>
2020-09-19 22:14:10 -0400

    Hotfix 91920

---
+++ src/Tonytins.Models/ArtworkBase.cs
@@ -0,0 +1,13 @@
+// Anthony Leland licenses this file to you under the MIT license.
+using System;
+
+namespace Tonytins.Models
+{
+    public class ArtworkBase
+    {
+        public string Image { get; set; }
+        public string Key { get; set; }
+        public string Credits { get; set; }
+        public string Copyrights { get; set; }
+    }
+}
--- src/Tonytins.Models/ArtworkModel.cs
+++ src/Tonytins.Models/ArtworkModel.cs
@@ -1,18 +1,10 @@
 // Anthony Leland licenses this file to you under the MIT license.
 namespace Tonytins.Models
 {
-    public class ArtworkModel
+    public class ArtworkModel : ArtworkBase
     {
         public string Title { get; set; }
 
         public string Description { get; set; }
-
-        public string Image { get; set; }
-
-        public string Credits { get; set; }
-
-        public string Copyrights { get; set; }
-
-        public string Key { get; set; }
     }
 }
